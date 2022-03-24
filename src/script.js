8f18cf9daadcd5becb4f6db541ecbe057741bd66
Twumasi Duoduaa Ama <91034181+AmaTwumasi@users.noreply.github.com>
2022-03-24 16:31:28 -0700

    Added API Integration

--- src/script.js
+++ src/script.js
@@ -1,24 +1,22 @@
-
-
 function showCity(event) {
   event.preventDefault();
   let city = document.querySelector("#change-city").value;
   let apiKey = "e5bb208507c0c4c97d696df0a3444983";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
- 
+
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${city}`;
 }
 let searchForm = document.querySelector("#city-form");
 searchForm.addEventListener("submit", showCity);
 
-function getForcast (coordinates) {
+function getForcast(coordinates) {
   console.log(coordinates);
-let apiKey = "e5bb208507c0c4c97d696df0a3444983";
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(forcastTemp);
+  let apiKey = "e5bb208507c0c4c97d696df0a3444983";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(forcastTemp);
 }
 
 function showTemp(response) {
@@ -34,19 +32,16 @@ function showTemp(response) {
   let currentDescription = response.data.weather[0].description;
   description.innerHTML = `${currentDescription}`;
   let currentIcon = document.querySelector("#icon");
- currentIcon.setAttribute(
-   "src",
-   `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
- );
- currentIcon.setAttribute("alt", response.data.weather[0].icon);
- celsiusTemp = response.data.main.temp;
- getForcast(response.data.coord);
-
+  currentIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  currentIcon.setAttribute("alt", response.data.weather[0].icon);
+  celsiusTemp = response.data.main.temp;
+  getForcast(response.data.coord);
 }
 
-
 function displayTemp(response) {
- 
   let temp = document.querySelector("#temperature");
   temp.innerHTML = Math.round(response.data.main.temp);
   let currentPlace = response.data.name;
@@ -59,20 +54,17 @@ function displayTemp(response) {
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `${currentHumidity}%`;
   let currentWind = Math.round(response.data.wind.speed);
-   let wind = document.querySelector("#wind");
+  let wind = document.querySelector("#wind");
   wind.innerHTML = `${currentWind}km/h`;
-    let currentIcon = document.querySelector("#icon");
-    currentIcon.setAttribute(
-      "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-    currentIcon.setAttribute(
-      "alt",
-      response.data.weather[0].icon
-    );
-     celsiusTemp = response.data.main.temp;
-     forcastTemp();
-     getForcast(response.data.coord);
+  let currentIcon = document.querySelector("#icon");
+  currentIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  currentIcon.setAttribute("alt", response.data.weather[0].icon);
+  celsiusTemp = response.data.main.temp;
+  forcastTemp();
+  getForcast(response.data.coord);
 }
 let currentLocation = document.querySelector("button");
 currentLocation.addEventListener("click", getPosition);
@@ -80,12 +72,11 @@ currentLocation.addEventListener("click", getPosition);
 //Bonus homework
 
 function showCoord(position) {
-
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiKey = "e5bb208507c0c4c97d696df0a3444983";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
- 
+
   axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemp);
 }
 function getPosition(position) {
@@ -118,33 +109,53 @@ let day = days[currentDate.getDay()];
 
 date.innerHTML = `${day} ${hour}:${minutes}`;
 
+function formateDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
 
-function forcastTemp (response) {
-console.log(response.data.daily);
-let forcast = document.querySelector("#weekdays")
-let forcastHTML = `<div class="row align-items-start">`;
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  
-];
-days.forEach (function(days) {
-  forcastHTML = forcastHTML + 
-`<div class="col">
-            <strong>${days}</strong>
-            <br />
-            <p>30°C</p>
-            Sunny ⛅
+function forcastTemp(response) {
+  let forcastDays = response.data.daily;
+  let forcast = document.querySelector("#weekdays");
+  let forcastHTML = `<div class="row align-items-start">`;
+
+  forcastDays.forEach(function (forcastDay, index) {
+    if (index < 6) {
+      forcastHTML =
+        forcastHTML +
+        `<div class="col">
+            <div class="forcast-weather-date">${formateDay(forcastDay.dt)}</div>
+            <img
+            src = "http://openweathermap.org/img/wn/${
+              forcastDay.weather[0].icon
+            }@2x.png"
+            alt=""
+            width="42"
+            />
+            </br>
+            <span class ="forcast-weather-temp-max">${Math.round(
+              forcastDay.temp.max
+            )}°</span>
+             <span class ="forcast-weather-temp-min">${Math.round(
+               forcastDay.temp.min
+             )}°</span>
+            
           </div>`;
+    }
+  });
 
-})
-
-    forcastHTML = forcastHTML + `</div>`;    
-forcast.innerHTML = forcastHTML
+  forcastHTML = forcastHTML + `</div>`;
+  forcast.innerHTML = forcastHTML;
 }
 
 //feature 2
@@ -156,20 +167,20 @@ function city(event) {
 }
 function converttofarenheit(event) {
   event.preventDefault();
- 
-  let temperatureElement = document.querySelector("#temperature")
+
+  let temperatureElement = document.querySelector("#temperature");
   let farenheitTemp = (celsiusTemp * 9) / 5 + 32;
 
   temperatureElement.innerHTML = Math.round(farenheitTemp);
 }
 function converttocelsius(event) {
   event.preventDefault();
- 
+
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemp);
 }
- 
-let celsiusTemp="null"
+
+let celsiusTemp = "null";
 let form = document.querySelector("#city-form");
 form.addEventListener("submit", city);
 
